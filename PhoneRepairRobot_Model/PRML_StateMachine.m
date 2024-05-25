4ffea15e8c974f92784ec8fc181955482d33b537
Rupom Saha <71686447+RupomSaha@users.noreply.github.com>
2024-05-25 20:38:45 +1000

    End of Week 24

--- PhoneRepairRobot_Model/PRML_StateMachine.m
+++ PhoneRepairRobot_Model/PRML_StateMachine.m
@@ -1,3 +1,11 @@
 % Define Phone Repair States
 Healthy_State = 1;
 Screen_Replacement = 2;
+Battery_Replacement = 3;
+ChargingPort_Rerplacement = 4;
+HomeButton_Replacement = 5;
+RearCamera_Replacement = 6;
+FrontCamers_Replacement = 7;
+ScreenProtector_Replacement = 8;
+Water_Damage = 9;
+FactoryReset = 10;
---
+++ ProblemIdentifyMachineLearning_Model/data_PRML.m
@@ -0,0 +1,18 @@
+close all;
+clear;
+clc;
+
+CrackScreen1 = imread("img_CrackedScreen\DataclusterCrackedScreen1.jpg");
+CrackScreen10 = imread("img_CrackedScreen\DataclusterCrackedScreen10.jpg");
+CrackScreen100 = imread("img_CrackedScreen\DataclusterCrackedScreen100.jpg");
+CrackScreen102 = imread("img_CrackedScreen\DataclusterCrackedScreen102.jpg");
+CrackScreen103 = imread("img_CrackedScreen\DataclusterCrackedScreen103.jpg");
+CrackScreen104 = imread("img_CrackedScreen\DataclusterCrackedScreen104.jpg");
+CrackScreen105 = imread("img_CrackedScreen\DataclusterCrackedScreen105.jpg");
+CrackScreen106 = imread("img_CrackedScreen\DataclusterCrackedScreen106.jpg");
+CrackScreen107 = imread("img_CrackedScreen\DataclusterCrackedScreen107.jpg");
+CrackScreen108 = imread("img_CrackedScreen\DataclusterCrackedScreen108.jpg");
+CrackScreen109 = imread("img_CrackedScreen\DataclusterCrackedScreen109.jpg");
+CrackScreen112 = imread("img_CrackedScreen\DataclusterCrackedScreen112.jpg");
+
+% CrackScreen_Imgdata
